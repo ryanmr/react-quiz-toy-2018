f67eb600765c5eef1ea07560fff365cfbdfcd5d8
Ryan Rampersad <ryan.rampersad@gmail.com>
2018-04-21 21:32:55 -0500

    Removed react spring, simple impl

--- src/Display.js
+++ src/Display.js
@@ -1,29 +1,19 @@
 import React, { Component } from "react";
-import {
-  Spring,
-  Trail,
-  Transition,
-  Keyframes,
-  animated,
-  config
-} from "react-spring";
 import delay from "delay";
 import _ from "lodash";
 
 class Display extends React.Component {
   state = {
     prompt: "?",
-    answer: "",
     options: [],
-    items: []
+    showAnswer: false
   };
 
   async componentDidMount() {
-    // Since the script prop basically just receives the animation function we can
-    // pick it up and use it elsewhere, that makes it possible to combine several of them
-    // even though they belong to different elements, they can wait for one another, etc.
-    // And again, since we are using the native prop none of this causes re-rendering
     while (true) {
+      this.setState({
+        showAnswer: false
+      });
       const quizlet = this.props.getQuizlet();
       console.log(quizlet);
 
@@ -31,136 +21,60 @@ class Display extends React.Component {
         value: option,
         answer: false
       }));
-      const items = [
-        ...falseOptions,
-        { value: quizlet.answer, answer: true }
-      ].map(v => {
-        v.toString = () => v.value;
-        return v;
-      });
+      const items = [...falseOptions, { value: quizlet.answer, answer: true }];
       const shuffledItems = _.shuffle(items);
       console.log(items);
 
       this.setState({
         prompt: quizlet.prompt,
-        items: shuffledItems
+        options: shuffledItems
       });
 
-      // set state will cause an render update
-      // but it will cause the animation to jitter
-      await delay(1000);
+      await delay(5000);
 
-      this.container(Spring, {
-        from: { x: -100 },
-        to: { x: 0 },
-        config: config.slow
-      });
-      await delay(1000);
-      await this.prompt(Spring, {
-        from: { x: -120, opacity: 0 },
-        to: { x: 0, opacity: 1 }
+      this.setState({
+        showAnswer: true
       });
 
-      this.content(Trail, {
-        from: { x: -120, opacity: 0 },
-        to: { x: 0, opacity: 1 }
-      });
-      await delay(100);
-      await this.answer(Spring, {
-        immediate: true,
-        to: { opacity: 1 }
-      });
-
-      await delay(4 * 1000);
-
-      await this.answer(Spring, {
-        from: {
-          backgroundColor: "#6154d8",
-          color: "#ececec",
-          border: "5px solid rgba(255,255,255,0)"
-        },
-        to: {
-          backgroundColor: "#4732ff",
-          color: "#fff",
-          border: "5px solid rgba(255,255,255,1)"
-        },
-        config: config.slow
-      });
-
-      await delay(4 * 1000);
-
-      await this.content(Trail, { to: { x: -120, opacity: 0 } });
-
-      await this.container(Spring, { to: { x: -100 }, config: config.slow });
+      await delay(5000);
     }
   }
 
   render() {
     return (
-      <Keyframes native script={next => (this.container = next)}>
-        {({ x }) => (
-          <animated.div
-            className="container"
-            style={{ transform: x.interpolate(x => `translate3d(${x}%,0,0)`) }}
-          >
-            <Keyframes native script={next => (this.prompt = next)}>
-              {({ x }) => (
-                <animated.div
-                  className="prompt"
-                  style={{
-                    transform: x.interpolate(x => `translate3d(${x}%,0,0)`)
-                  }}
-                >
-                  {this.state.prompt}
-                </animated.div>
-              )}
-            </Keyframes>
-            <Keyframes
-              native
-              keys={this.state.items}
-              script={next => (this.content = next)}
+      <div className="container">
+        <div className="prompt">{this.state.prompt}</div>
+        <div className="options">
+          {this.state.options.map(({ value, answer }) => (
+            <div
+              className="option item"
+              key={value}
+              style={{
+                ...(this.state.showAnswer && answer
+                  ? styles.rightAnswer
+                  : undefined),
+                ...(this.state.showAnswer && !answer
+                  ? styles.wrongAnswer
+                  : undefined)
+              }}
             >
-              {this.state.items.map(item => ({ x, ...props }) => {
-                const { answer } = item;
-                if (answer) {
-                  return (
-                    <Keyframes native script={next => (this.answer = next)}>
-                      {({ ...answerProps }) => {
-                        return (
-                          <animated.div
-                            className="item"
-                            style={{
-                              transform: x.interpolate(
-                                x => `translate3d(${x}%,0,0)`
-                              ),
-                              ...answerProps
-                            }}
-                          >
-                            {item.value}
-                          </animated.div>
-                        );
-                      }}
-                    </Keyframes>
-                  );
-                }
-                return (
-                  <animated.div
-                    className="item"
-                    style={{
-                      transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
-                      ...props
-                    }}
-                  >
-                    {item.value}
-                  </animated.div>
-                );
-              })}
-            </Keyframes>
-          </animated.div>
-        )}
-      </Keyframes>
+              {value}
+            </div>
+          ))}
+        </div>
+      </div>
     );
   }
 }
+
+const styles = {
+  rightAnswer: {
+    backgroundColor: "#00f",
+    color: "#fff"
+  },
+  wrongAnswer: {
+    opacity: 0.75
+  }
+};
 
 export default Display;
